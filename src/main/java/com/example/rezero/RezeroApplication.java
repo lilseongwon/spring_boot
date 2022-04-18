c357d105381d23028efc5e2f74cee97c5a03c8b1
kim seongwon <seongwon1290@dsm.hs.kr>
2022-04-18 21:32:08 +0900

    spring 2.1 ~ 3.2

---
+++ src/main/java/com/example/rezero/RezeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.rezero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RezeroApplication.class, args);
+    }
+
+}
