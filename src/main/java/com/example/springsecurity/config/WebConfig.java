8bc20e13a438ed5eeeef31484010dbe56c1f4eff
kim seongwon <seongwon1290@dsm.hs.kr>
2022-06-11 21:06:03 +0900

    🎈::CORS 설정

--- src/main/java/com/example/springsecurity/config/WebConfig.java
+++ src/main/java/com/example/springsecurity/config/WebConfig.java
@@ -1,12 +1,19 @@
 package com.example.springsecurity.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:9089");
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT")
+                    .allowedOrigins("http://localhost:9089");
+        }
+
     }
-}
+
