8a94452d5e61fb152ef4c7e07f6a723224a0d5c5
kim seongwon <seongwon1290@dsm.hs.kr>
2022-06-14 23:03:24 +0900

    💊::super 주석

--- src/main/java/com/example/springsecurity/exception/AlreadyExistEmailException.java
+++ src/main/java/com/example/springsecurity/exception/AlreadyExistEmailException.java
@@ -9,5 +9,7 @@ public class AlreadyExistEmailException extends CustomException {
 
     private AlreadyExistEmailException() {
         super(ErrorCode.AUTH_EXISTS);
+        //부모클래스의 멤버를 초기화하기 위해선, 당연히 부모클래스의 생성자를 호출해야할 것이다.
+        //매개변수를 가지는 생성자기 때문에 super로 cutomException(부모클래스)의 에러코드를 원하는 에러로 초기화시킨다.
     }
 }
