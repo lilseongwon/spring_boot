88dbbd2a4d314260c7f0719912d434cfb7136635
kim seongwon <seongwon1290@dsm.hs.kr>
2022-06-16 00:15:27 +0900

    🔬::검증 어노테이션 추가

--- src/main/java/com/example/springsecurity/dto/AccountForm.java
+++ src/main/java/com/example/springsecurity/dto/AccountForm.java
@@ -7,16 +7,27 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor
 public class AccountForm {
+    @NotEmpty(message = "공백을 허용하지 않습니다.")
     private String accountId;
-    @NotNull
+
+    @NotEmpty(message = "공백을 허용하지 않습니다.")
     private String password;
+
+    @NotEmpty(message = "공백을 허용하지 않습니다.")
     private String email;
+
+    @NotEmpty(message = "공백을 허용하지 않습니다.")
     private String name;
+
+    @NotEmpty(message = "공백을 허용하지 않습니다.")
     private String student_id;
+
+    @NotEmpty(message = "공백을 허용하지 않습니다.")
     private Sex sex;
 }
